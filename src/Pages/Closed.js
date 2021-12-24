0cc996c73c3e1feec7ea2a1b6643ecf2f652b22c
Christopher Rose <christophervrose@gmail.com>
2021-12-24 08:24:33 +0530

    [add] closed graphs

--- src/Pages/Closed.js
+++ src/Pages/Closed.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -8,68 +8,133 @@ import { useEffect } from "react";
 import DatePicker from "react-date-picker";
 import ClosedPieGraph from "../components/Graphs/ClosedPieGraph";
 import LineChart from "../components/Graphs/LineGraph";
+import { SelectedContext } from "../etc/context";
+import axios from "axios";
+import api_link from "../etc/api";
 function Closed() {
-  const [value, setValue] = React.useState(new Date());
-  const yearDecided = useMemo(()=>{return value.getFullYear()},[value])
-  const monthDecided = useMemo(()=>{return value.getMonth()},[value])
+    const [value, setValue] = React.useState(new Date());
+    const year = useMemo(() => {
+        return value.getFullYear();
+    }, [value]);
+    const month = useMemo(() => {
+        return value.getMonth();
+    }, [value]);
 
-  // const FetchData = (sele) => {
-  //   console.log(selectedOption.id);
-  //   axios
-  //     .get(`http://172.16.18.42:3001/api/models/preds/${selectedOption}`, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: "Bearer " + localStorage.getItem("token"),
-  //       },
-  //     })
-  //     .then((res) => {
-  //       setGlobalSelected(res.data);
-  //     });
-  // };
-  return (
-    <>
-      <div className="flex flex-row items-center justify-center m-5 p-5">
-        <DatePicker
-          onChange={(date) => setValue(date)}
-          value={value}
-          format="MM/yyyy"
-          maxDetail='year'
-        />
-      </div>
-      <div className="grid grid-flow-col grid-cols-1 lg:grid-cols-2">
-        <div className="bg-gray-50 p-5 m-2">
-          <p>
-            <span className="text-gray-500">Expense :</span>
-          </p>
+    // MAKE THE DATA
+    const [alldata, setAlldata] = useState([]);
+    const token = useMemo(() => localStorage.getItem("token"), []);
+    const [id, setId] = useContext(SelectedContext);
 
-          <ClosedPieGraph height={200} width={200} year={yearDecided} month={monthDecided} />
-        </div>
-        <div className="bg-gray-50 p-5 m-2">
-          <p>
-            <span className="text-gray-500">Expense :</span>
-          </p>
-          <LineChart height={200} width={200} />
-          <div className="m-5" />
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Age</InputLabel>
-              {/* <Select
+    useEffect(() => {
+        axios
+            .get(`${api_link}/api/models/preds/1`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((res) => {
+                const data = res.data;
+                setAlldata(data);
+            });
+    }, [id, token]);
+    // trimmed part of data
+    const trimmedData = useMemo(() => {
+        const data = alldata.filter((e) => {
+            const dateString = e["Enquiry Date"];
+            const datesplit = dateString.split("-");
+            const dateyear = parseInt(datesplit[2]);
+            const datemonth = parseInt(datesplit[1]);
+            const pred = year === dateyear && month === datemonth;
+            // console.log('Try',year,month,datesplit,pred);
+            return pred;
+        });
+        console.log("trimmed data", data);
+        return data;
+    }, [year, month, alldata]);
+    const { chartjsoptions} = useMemo(() => {
+        const total = new Map();
+        trimmedData.forEach((element) => {
+            total.set(
+                element["Predicted Prob.1"],
+                (total.get(element["Predicted Prob.1"]) ?? 0) + 1
+            );
+        });
+        const trimArray = [...total];
+        const labels = trimArray.map((e) => e[0]);
+        const counts = trimArray.map((e) => e[1]);
+        const chartjsoptions = {
+            labels,
+            datasets: [
+                {
+                    label: "My First Dataset",
+                    data: counts,
+                    backgroundColor: [
+                        "rgb(255, 99, 132)",
+                        "rgb(54, 162, 235)",
+                        "rgb(255, 205, 86)",
+                        "#2E7500",
+                        "#839DD0",
+                        "#007280",
+                        "#7985C3",
+                        "#EB9E64",
+                        "#D2D4C0",
+                        "#AEA306",
+                    ],
+                    hoverOffset: 4,
+                },
+            ],
+        };
+        return {labels, counts, chartjsoptions};
+    }, [trimmedData]);
+    console.log('log',chartjsoptions);
+    return (
+        <>
+            <div className="flex flex-row items-center justify-center m-5 p-5">
+                <DatePicker
+                    onChange={(date) => setValue(date)}
+                    value={value}
+                    format="MM/yyyy"
+                    maxDetail="year"
+                />
+            </div>
+            <div className="grid grid-flow-col grid-cols-1 lg:grid-cols-2">
+                <div className="bg-gray-50 p-5 m-2">
+                    <p>
+                        <span className="text-gray-500">Expense :</span>
+                    </p>
+
+                    <ClosedPieGraph
+                        height={200}
+                        width={200}
+                        chartjsdata={chartjsoptions}
+                    />
+                </div>
+                <div className="bg-gray-50 p-5 m-2">
+                    <p>
+                        <span className="text-gray-500">Expense :</span>
+                    </p>
+                    <LineChart height={200} width={200} />
+                    <div className="m-5" />
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">
+                                Age
+                            </InputLabel>
+                            {/* <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 value={age}
                 label="Age"
                 onChange={handleChange}
               > */}
-              {/* <MenuItem value={10}>Ten</MenuItem>
+                            {/* <MenuItem value={10}>Ten</MenuItem>
                 <MenuItem value={20}>Twenty</MenuItem>
                 <MenuItem value={30}>Thirty</MenuItem>
               </Select> */}
-            </FormControl>
-          </Box>
-        </div>
-      </div>
-    </>
-  );
+                        </FormControl>
+                    </Box>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Closed;
--- src/components/Graphs/ClosedPieGraph.js
+++ src/components/Graphs/ClosedPieGraph.js
@@ -9,10 +9,9 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 /**
  *
- * @param {{year:number,month:number,status:string}} param0
  * @returns
  */
-export default function PieChart({ year, month, status }) {
+export default function PieChart({ chartjsdata }) {
     const options = {
         responsive: true,
         interaction: {
@@ -42,89 +41,12 @@ export default function PieChart({ year, month, status }) {
             },
         },
     };
-    // const data = {
-    //     labels: ["Red", "Blue", "Yellow"],
-    //     datasets: [
-    //         {
-    //             label: "My First Dataset",
-    //             data: [300, 50, 100],
-    //             backgroundColor: [
-    //                 "rgb(255, 99, 132)",
-    //                 "rgb(54, 162, 235)",
-    //                 "rgb(255, 205, 86)",
-    //             ],
-    //             hoverOffset: 4,
-    //         },
-    //     ],
-    // };
 
-    const [alldata, setAlldata] = useState([]);
-    const token = useMemo(() => localStorage.getItem("token"), []);
-    const [id, setId] = useContext(SelectedContext);
-
-    useEffect(() => {
-        axios
-            .get(`${api_link}/api/models/preds/1`, {
-                headers: { Authorization: `Bearer ${token}` },
-            })
-            .then((res) => {
-                const data = res.data;
-                setAlldata(data);
-            });
-    }, [id, token]);
-
-    const trimmedData = useMemo(() => {
-        const data = alldata.filter((e) => {
-            const dateString = e["Enquiry Date"];
-            const datesplit = dateString.split("-");
-            const dateyear = parseInt(datesplit[2]);
-            const datemonth = parseInt(datesplit[1]);
-            const pred = year === dateyear && month === datemonth;
-            // console.log('Try',year,month,datesplit,pred);
-            return pred;
-        });
-        console.log("trimmed data", data);
-        return data;
-    }, [year, month, alldata]);
-    const [labels,chartcounts,chartjsdata] = useMemo(() => {
-        const total = new Map();
-        const trimmedCounts = trimmedData.forEach((element) => {
-            
-            total.set(
-                element["Predicted Prob.1"],
-                (total.get(element["Predicted Prob.1"]) ?? 0) + 1
-            );
-        });
-        const trimArray = [...total];
-        const labels = trimArray.map(e=>e[0]);
-        const counts = trimArray.map(e=>e[1]);
-        const chartjsoptions = {
-            labels,
-            datasets: [
-              {
-                label: "My First Dataset",
-                data: counts,
-                backgroundColor: [
-                  "rgb(255, 99, 132)",
-                  "rgb(54, 162, 235)",
-                  "rgb(255, 205, 86)",
-                  "#2E7500",
-                  "#839DD0",
-                  "#007280",
-                  "#7985C3",
-                  "#EB9E64",
-                  '#D2D4C0',
-                  '#AEA306',
-                ],
-                hoverOffset: 4,
-              },
-            ]};
-        return [labels,counts,chartjsoptions];
-    }, [trimmedData]);
+    
 
     return (
         <>
-            <Doughnut data={chartjsdata} opions={options} />
+            <Doughnut data={chartjsdata}  />
         </>
     );
 }
