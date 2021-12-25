5cbfcaabdab15dc891f931186ea062f9950ba177
Prajwal P <prajwalprakash3722@gmail.com>
2021-12-25 20:35:32 +0530

    [add] We can Add models now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import LoginPage from "./Pages/Login";
-import RegisterPage from "./Pages/Register";
-import ClosedLeadsPage from "./Pages/Closed";
-import AllLeadsPage from "./Pages/AllLeads";
-import TablesPage from "./Pages/TablesPage";
-import PredTablesPage from "./Pages/PredTablesPage";
-import ProfilePage from "./Pages/Profile";
-import Add from "./Pages/Add";
-import NavBar from "./components/NavBar";
-import { SelectedContext } from "./etc/context";
+import LoginPage from './Pages/Login';
+import RegisterPage from './Pages/Register';
+import ClosedLeadsPage from './Pages/Closed';
+import AllLeadsPage from './Pages/AllLeads';
+import TablesPage from './Pages/TablesPage';
+import PredTablesPage from './Pages/PredTablesPage';
+import ProfilePage from './Pages/Profile';
+import AddModel from './Pages/AddModel';
+import Add from './Pages/Add';
+import NavBar from './components/NavBar';
+import { SelectedContext } from './etc/context';
 function App() {
-    const [token, setToken] = useState("");
+    const [token, setToken] = useState('');
     const selectedState = useState(null);
     useEffect(() => {
-        setToken(localStorage.getItem("token"));
+        setToken(localStorage.getItem('token'));
     }, [token]);
     return (
         <>
@@ -29,7 +30,12 @@ function App() {
                         <Route path="/register" element={<RegisterPage />} />
                         <Route path="/allleads" element={<AllLeadsPage />} />
                         <Route path="/tables" element={<TablesPage />} />"
-                        <Route path="/predtables" element={<PredTablesPage />} />"
+                        <Route
+                            path="/predtables"
+                            element={<PredTablesPage />}
+                        />
+                        "
+                        <Route path="/addmodel" element={<AddModel />} />
                         <Route
                             path="/closedleads"
                             element={<ClosedLeadsPage />}
